196c7f42ff8629a907037c5e23966ca5dc44e208
Jorgi <jorgi1tovar@gmail.com>
2024-11-25 00:55:56 -0600

    Initial Commit

---
+++ arTest/arTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  arTestApp.swift
+//  arTest
+//
+//  Created by Jorge Tovar on 25/11/24.
+//
+
+import SwiftUI
+
+@main
+struct arTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ arTestTests/arTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  arTestTests.swift
+//  arTestTests
+//
+//  Created by Jorge Tovar on 25/11/24.
+//
+
+import Testing
+@testable import arTest
+
+struct arTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
